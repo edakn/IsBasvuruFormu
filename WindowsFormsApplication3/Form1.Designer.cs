d83ea2f73733acb670cd4eb9dfa6c5c03e7caa78
edakn <73113871+edakn@users.noreply.github.com>
2022-11-08 01:18:12 +0300

    Job Application Form

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataSet11 = new WindowsFormsApplication3.DataSet1();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.oleDbSelectCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            this.oleDbInsertCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbDataAdapter1 = new System.Data.OleDb.OleDbDataAdapter();
+            this.oleDbCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbCommand2 = new System.Data.OleDb.OleDbCommand();
+            this.dataView1 = new System.Data.DataView();
+            this.dataGrid1 = new System.Windows.Forms.DataGrid();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC_Kimlik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Eğitim Düzeyi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Askerlik Yaptın mı?";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataSet11, "kisib.Tc_Kimlik", true));
+            this.textBox1.Location = new System.Drawing.Point(114, 20);
+            this.textBox1.MaxLength = 11;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // dataSet11
+            // 
+            this.dataSet11.DataSetName = "DataSet1";
+            this.dataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataSet11, "kisib.Adı", true));
+            this.textBox2.Location = new System.Drawing.Point(114, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataSet11, "kisib.Soyadı", true));
+            this.textBox3.Location = new System.Drawing.Point(114, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataSet11, "kisib.Eğitim_Düzeyi", true));
+            this.textBox4.Location = new System.Drawing.Point(114, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataSet11, "kisib.Askerlik Yapıldı_mı?", true));
+            this.textBox5.Location = new System.Drawing.Point(114, 127);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Yeni";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Başvuru Yap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // oleDbSelectCommand1
+            // 
+            this.oleDbSelectCommand1.CommandText = "SELECT        Tc_Kimlik, Adı, Soyadı, [Eğitim_Düzeyi], [Askerlik Yapıldı_mı?]\r\nFR" +
+    "OM            kisib";
+            this.oleDbSelectCommand1.Connection = this.oleDbConnection1;
+            // 
+            // oleDbConnection1
+            // 
+            this.oleDbConnection1.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\vt1.mdb;Persist Secu" +
+    "rity Info=False";
+            // 
+            // oleDbInsertCommand1
+            // 
+            this.oleDbInsertCommand1.CommandText = "INSERT INTO `kisib` (`Tc_Kimlik`, `Adı`, `Soyadı`, `Eğitim_Düzeyi`, `Askerlik Yap" +
+    "ıldı_mı?`) VALUES (?, ?, ?, ?, ?)";
+            this.oleDbInsertCommand1.Connection = this.oleDbConnection1;
+            this.oleDbInsertCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Tc_Kimlik", System.Data.OleDb.OleDbType.VarWChar, 0, "Tc_Kimlik"),
+            new System.Data.OleDb.OleDbParameter("Adı", System.Data.OleDb.OleDbType.VarWChar, 0, "Adı"),
+            new System.Data.OleDb.OleDbParameter("Soyadı", System.Data.OleDb.OleDbType.VarWChar, 0, "Soyadı"),
+            new System.Data.OleDb.OleDbParameter("Eğitim_Düzeyi", System.Data.OleDb.OleDbType.VarWChar, 0, "Eğitim_Düzeyi"),
+            new System.Data.OleDb.OleDbParameter("Askerlik_Yapıldı_mı?", System.Data.OleDb.OleDbType.VarWChar, 0, "Askerlik Yapıldı_mı?")});
+            // 
+            // oleDbDataAdapter1
+            // 
+            this.oleDbDataAdapter1.DeleteCommand = this.oleDbCommand1;
+            this.oleDbDataAdapter1.InsertCommand = this.oleDbInsertCommand1;
+            this.oleDbDataAdapter1.SelectCommand = this.oleDbSelectCommand1;
+            this.oleDbDataAdapter1.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "kisib", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("Tc_Kimlik", "Tc_Kimlik"),
+                        new System.Data.Common.DataColumnMapping("Adı", "Adı"),
+                        new System.Data.Common.DataColumnMapping("Soyadı", "Soyadı"),
+                        new System.Data.Common.DataColumnMapping("Eğitim_Düzeyi", "Eğitim_Düzeyi"),
+                        new System.Data.Common.DataColumnMapping("Askerlik Yapıldı_mı?", "Askerlik Yapıldı_mı?")})});
+            this.oleDbDataAdapter1.UpdateCommand = this.oleDbCommand2;
+            // 
+            // oleDbCommand1
+            // 
+            this.oleDbCommand1.CommandText = "DELETE FROM kisib\r\nWHERE        (Tc_Kimlik = ?)";
+            this.oleDbCommand1.Connection = this.oleDbConnection1;
+            this.oleDbCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Tc_Kimlik", System.Data.OleDb.OleDbType.WChar, 50, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Tc_Kimlik", System.Data.DataRowVersion.Original, null)});
+            // 
+            // oleDbCommand2
+            // 
+            this.oleDbCommand2.CommandText = "UPDATE kisib SET Adı=?,Soyadı=?,Askerlik Yapıl=? WHERE Tc_Kimlik=?";
+            this.oleDbCommand2.Connection = this.oleDbConnection1;
+            // 
+            // dataView1
+            // 
+            this.dataView1.Table = this.dataSet11.kisib;
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.DataMember = "";
+            this.dataGrid1.DataSource = this.dataView1;
+            this.dataGrid1.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+            this.dataGrid1.Location = new System.Drawing.Point(12, 195);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(418, 141);
+            this.dataGrid1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 29);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Değişir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(260, 122);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 29);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(260, 157);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(69, 32);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Çıkış";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(69, 162);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(28, 23);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "|<";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(103, 162);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(30, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "<";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(139, 162);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(31, 23);
+            this.button8.TabIndex = 18;
+            this.button8.Text = ">";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(176, 162);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(32, 23);
+            this.button9.TabIndex = 19;
+            this.button9.Text = ">|";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 375);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGrid1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "İş Başvuru Formu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Data.OleDb.OleDbCommand oleDbSelectCommand1;
+        private System.Data.OleDb.OleDbConnection oleDbConnection1;
+        private System.Data.OleDb.OleDbCommand oleDbInsertCommand1;
+        private System.Data.OleDb.OleDbDataAdapter oleDbDataAdapter1;
+        private System.Data.DataView dataView1;
+        private DataSet1 dataSet11;
+        private System.Windows.Forms.DataGrid dataGrid1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Data.OleDb.OleDbCommand oleDbCommand1;
+        private System.Data.OleDb.OleDbCommand oleDbCommand2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,131 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        oleDbDataAdapter1.Fill(dataSet11, "kisib");
+        
+
+        
+        
+        }
+     
+        private void button1_Click(object sender, EventArgs e)
+        {          
+            
+            this.BindingContext[dataSet11, "kisib"].AddNew();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.Compare(textBox5.Text, "evet", true) == 0)
+                {
+                    this.BindingContext[dataSet11, "kisib"].EndCurrentEdit(); 
+                    oleDbDataAdapter1.Update(dataSet11); 
+                    
+                    MessageBox.Show("İstenen Özelliklere Uyuyorsunuz");
+                }
+                else
+                {
+                    MessageBox.Show("Askerlik yapmadığınızdan iş başvurunuz değerlendirilmeye alınmayacak");
+                    
+                }
+            }
+            catch
+            {
+                ;
+            }
+
+
+
+        }
+      
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                this.BindingContext[dataSet11, "kisib"].EndCurrentEdit(); 
+                oleDbDataAdapter1.Update(dataSet11); 
+
+            }
+            catch
+            {
+                ;
+                 
+            }
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.BindingContext[dataSet11, "kisib"].EndCurrentEdit(); 
+                this.BindingContext[dataSet11, "kisib"].RemoveAt(this.BindingContext[dataSet11, "kisib"].Position); 
+                oleDbDataAdapter1.Update(dataSet11); 
+
+            }
+            catch
+            {
+                ;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+      
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.BindingContext[dataSet11, "kisib"].Position = 0;
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.BindingContext[dataSet11, "kisib"].Position -= 1;
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.BindingContext[dataSet11, "kisib"].Position += 1;
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.BindingContext[dataSet11, "kisib"].Position = this.BindingContext[dataSet11, "kisib"].Count;
+            
+        }
+    }
+}
